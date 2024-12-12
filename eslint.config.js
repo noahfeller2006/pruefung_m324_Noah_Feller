618f30d8707c6085e82af9bb458f8cebd4edcc37
Noah Feller <noah.feller@gmx.ch>
2024-12-12 14:32:09 +0100

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    languageOptions: {
-      globals: globals.browser 
-    }
+    languageOptions:
+      {
+        globals:
+          globals.browser,
+      },
   },
-  pluginJs.configs.recommended,
+  pluginJs
+    .configs
+    .recommended,
   {
-    rules: {
-      eqeqeq: ['error', 'always'], // Enforce === and !== over == and !=
-      "prefer-const": ["error", {
-        "destructuring": "all",
-        "ignoreReadBeforeAssign": false
-    }]
-    },
+    rules:
+      {
+        eqeqeq:
+          [
+            'error',
+            'always',
+          ], // Enforce === and !== over == and !=
+        'prefer-const':
+          [
+            'error',
+            {
+              destructuring:
+                'all',
+              ignoreReadBeforeAssign: false,
+            },
+          ],
+      },
   },
 ];
