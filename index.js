acf9e99fe7a86873b372e5bcd1b2e952bafacecf
Noah Feller <noah.feller@gmx.ch>
2024-12-12 14:30:02 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import chalk from "chalk"
 
 console.log(chalk.red('It works'))
 
-let myUnusedVariable = "nothing";
+const myUnusedVariable = "nothing";
+console.log(myUnusedVariable)
 
 const x = 10
-if (10 == x) {
+if (10 === x) {
     console.log("10")
 }
